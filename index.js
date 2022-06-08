1447ebdba456584b8230aec8992098ecf7a14e8a
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-08 21:42:21 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -8,8 +8,3 @@ function submit() {
     alert('Visit my github page https://github.com/Thee-pete?tab=repositories');
     }
     document.getElementById("viewBtn").addEventListener("click", hello);
-    $(document).ready(function(){
-        $("form").submit(function(){
-            alert("Your message has been submitted. We will reply as soon as possible");
-        });
-    });
